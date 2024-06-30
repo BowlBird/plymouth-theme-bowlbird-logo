ba8ee5c67f7f8317947686116d5015d162460e3c
BowlBird <bowlbirdcontact@gmail.com>
2024-06-30 10:08:41 -0500

    fixed flake definition

--- flake.nix
+++ flake.nix
@@ -2,26 +2,38 @@
   description = "BowlBird Logo Plymouth Theme";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs= nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, flake-utils }:
+    let allSystems = flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
 
+        package = pkgs.stdenv.mkDerivation {
+          pname = "plymouth-theme-bowlbird-logo";
+          version = "2.0.0";
+          src = ./bowlbird-logo;
+
+          installPhase = ''
+            mkdir -p $out/share/plymouth/themes/bowlbird-logo
+            cp * $out/share/plymouth/themes/bowlbird-logo
+            chmod +x $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.plymouth $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.script
+            sed -i "s@/usr/@$out/@" $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.plymouth
+          '';
+        };
+      in {
+        packages.plymouth-theme-bowlbird-logo = package;
+        defaultPackage = package;
+      }
+    );
     in {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        pname = "plymouth-theme-bowlbird-logo";
-        version = "2.0.0";
-        src = ./bowlbird-logo;
+      packages = allSystems.packages;
+      defaultPackage = allSystems.defaultPackage; 
 
-        installPhase = ''
-          mkdir -p $out/share/plymouth/themes/bowlbird-logo
-          cp * $out/share/plymouth/themes/bowlbird-logo
-          chmod +x $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.plymouth $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.script
-          sed -i "s@/usr/@$out/@" $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.plymouth
-        '';
+      overlay = final: prev: {
+        plymouth-theme-bowlbird-logo = 
+          allSystems.packages.${final.system}.plymouth-theme-bowlbird-logo;
       };
     };
 }
