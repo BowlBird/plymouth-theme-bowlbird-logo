0825e0917cec90c5b77060f1342b9c27a3d81845
BowlBird <bowlbirdcontact@gmail.com>
2024-06-30 09:45:30 -0500

    fixed sed

--- flake.nix
+++ flake.nix
@@ -13,14 +13,14 @@
     in {
       packages.default = pkgs.stdenv.mkDerivation {
         pname = "plymouth-theme-bowlbird-logo";
-        version = "1.0.0";
+        version = "2.0.0";
         src = ./bowlbird-logo;
 
         installPhase = ''
           mkdir -p $out/share/plymouth/themes/bowlbird-logo
           cp * $out/share/plymouth/themes/bowlbird-logo
           chmod +x $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.plymouth $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.script
-          sed -i "s@/usr/@$out/@" $out/share/plymouth/themes/nixos-load/nixos-load.plymouth
+          sed -i "s@/usr/@$out/@" $out/share/plymouth/themes/bowlbird-logo/bowlbird-logo.plymouth
         '';
       };
     };
